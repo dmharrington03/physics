d40ac4f11021059719d9b5aae73abbd826bd936c
Daniel Harrington <dmharrington03@gmail.com>
2021-06-06 22:45:39 +0800

    Curve and animation run on same timestep

--- Kinematics-2D/src/CurveArray.cpp
+++ Kinematics-2D/src/CurveArray.cpp
@@ -1,15 +1,14 @@
-std::vector<sf::Vector2f> createPointArray(sf::Vector2f vel, double g, double dt) {
+std::vector<sf::Vector2f> createPointArray(sf::Vector2f vel, double g, double dt, double scaleFactor) {
 	/* Creates and returns a list of relative coordinates according to
 	a parametric function in terms of x and y */
-	double t = 0.0;
-	double scaleFactor = 50.0;
 	sf::Vector2f scaledVel(vel.x / scaleFactor, vel.y / scaleFactor);
 	std::vector<sf::Vector2f> coords;
 	coords.push_back(sf::Vector2f(0.0, 0.0));
-	while (!(coords.back().y < 0.0))
+	for (double t = 0.0, i = 0.0; t < (2 * scaledVel.y / -g); t += dt, i++)
 	{
 		t += dt;
-		coords.push_back(sf::Vector2f(scaledVel.x * t, scaledVel.y * t + 0.5 * g * std::pow(t, 2)));
+		if ((int) i % 100 == 0)
+			coords.push_back(sf::Vector2f(scaledVel.x * t, scaledVel.y * t + 0.5 * g * std::pow(t, 2)));
 	}
 	return coords;
 }

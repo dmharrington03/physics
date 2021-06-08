9cd362c2336d7701dd0f939b00c8eb909ad6c86a
Daniel Harrington <dmharrington03@gmail.com>
2021-06-08 18:32:58 +0800

    Responsive header

--- docs/App.js
+++ docs/App.js
@@ -18,17 +18,37 @@ fetch("/markdown/kinematics-1d.md")
     .catch((error) => console.log(error));
 
 
-function NavHeader() {
-    const out = <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
-                    <nav id="nav-bg" className="uk-navbar-container uk-light uk-fixed">
-                        <div className="uk-navbar-center">
-                            <a href="" className="uk-navbar-item uk-logo">Computational Physics</a>
-                        </div>
-                    </nav>
-                </div>;
+function NavHeader(props) {
+
+    const bgStyle = {
+        backgroundImage: `linear-gradient(to right, rgba(50, 50, 50, 0), rgba(20, 20, 20, .8)), 
+                    url('img/img${ props.index }.png')`,
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed'
+    };
+    
+    const out = 
+    <div>
+        <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
+            <nav id="nav-bg" className="uk-navbar-container uk-light uk-fixed">
+                <div className="uk-navbar-center">
+                    <a href="" className="uk-navbar-item uk-logo">Computational Physics</a>
+                </div>
+            </nav>
+        </div>
+        <div id="header-bg" 
+            className="uk-background-cover uk-background-top-left
+            uk-height-large 
+            uk-light uk-flex uk-flex-center uk-flex-middle" 
+            uk-parallax="bgy: 0, -150;"
+            style={ bgStyle }>
+            <h1>{ props.title }</h1> 
+        </div>
+    </div>;
+    
     return out;
 }
 
 
-ReactDOM.render(<NavHeader />, document.getElementById('head'));
+ReactDOM.render(<NavHeader title="Kinematics - 1D" index="0"/>, document.getElementById('head'));
 UIkit.navbar(document.getElementById('nav-bg'), {});

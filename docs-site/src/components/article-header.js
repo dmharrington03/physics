dca5f1f1747643913f2b41809608491a6f9e0a2b
Daniel Harrington <dmharrington03@gmail.com>
2021-06-13 13:44:08 +0800

    Initial commit

---
+++ docs-site/src/components/article-header.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+const ArticleHeader = ({imgURL, title}) => {
+  const bgStyle = {
+    backgroundImage: `linear-gradient(to right, rgba(50, 50, 50, 0), rgba(20, 20, 20, .8)), 
+                url('${ imgURL }')`,
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed'
+  };
+
+  const out = 
+  <div>
+      <div id="header-bg" 
+          className="uk-background-cover uk-background-top-left
+          uk-height-large 
+          uk-light uk-flex uk-flex-center uk-flex-middle" 
+          uk-parallax="bgy: 0, -150;"
+          style={ bgStyle }>
+          <h1>{ title }</h1> 
+      </div>
+  </div>;
+
+  return out;
+}
+
+export default ArticleHeader;

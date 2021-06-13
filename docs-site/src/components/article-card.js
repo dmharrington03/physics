7a24eac11cb39fa22deb1e681a63a769e135fe9c
Daniel Harrington <dmharrington03@gmail.com>
2021-06-13 13:43:57 +0800

    Now takes in image prop to display

--- docs-site/src/components/article-card.js
+++ docs-site/src/components/article-card.js
@@ -1,18 +1,18 @@
 import * as React from "react";
 
-export default function ArticleCard(props) {
+export default function ArticleCard({title, to, imgURL}) {
   const format = (
     <div
       className="uk-card uk-card-default uk-card-hover 
                             uk-padding-remove"
     >
       <div className="uk-card-media-top">
-        <img src="" height="auto" />
+        <img src={ imgURL } height="auto" />
       </div>
       <div className="uk-card-body uk-margin-remove-top">
-        <div className="uk-grid-small uk-flex-middle" uk-grid="true">
+        <div className="uk-grid-small uk-flex-middle" data-uk-grid="true">
           <div className="uk-width-expand">
-            <h3 className="uk-card-title uk-margin-remove">{props.title}</h3>
+            <h3 className="uk-card-title uk-margin-remove">{ title }</h3>
             <p className="uk-text-meta uk-margin-remove-top">
               <time dateTime="2016-04-01T19:00">April 01, 2016</time>
             </p>
@@ -20,7 +20,7 @@ export default function ArticleCard(props) {
         </div>
       </div>
       <div className="uk-card-footer">
-        <a href={props.to} className="uk-button uk-button-text">
+        <a href={ to } className="uk-button uk-button-text">
           Read more
         </a>
       </div>

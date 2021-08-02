c4127faf11c50d31651346451e6684a77ef6b82c
Daniel Harrington <dmharrington03@gmail.com>
2021-08-01 22:22:29 -0400

    Expanded class members

--- Gravity/src/Header.hpp
+++ Gravity/src/Header.hpp
@@ -1,13 +1,20 @@
 class Planet : public sf::CircleShape
 {
-	private:
-
-		double mass;
-		int radius;
-
 	public:
 
-		static double ComputeDistance(sf::Vector2f pos1, sf::Vector2f pos2);
+		sf::Vector2f velocity;
+		double mass;
+		static double ComputeDistance(const sf::Vector2f pos1, const sf::Vector2f pos2);
 
-		Planet(double m, int rad);
+		void doMotion(const float& dt);
+
+		Planet(const sf::Vector2f pos, const double m, const int rad) : sf::CircleShape(rad), velocity(0, 0), mass(m)
+		{
+			Planet::setPosition(pos);
+			Planet::setFillColor(sf::Color(245, 225, 70));
+			Planet::setOrigin(sf::Vector2f(Planet::getRadius(), Planet::getRadius()));
+			std::cout << Planet::mass;
+		};
 };
+
+double Update(Planet* body, const Planet* sun, const float& dt);

9f58f303488acf2562fb9a3f9004d087ed1db8fc
Daniel Harrington <dmharrington03@gmail.com>
2021-09-11 13:04:54 -0400

    2D rotation matrix

--- 3D-Rendering/src/3DUtility.hpp
+++ 3D-Rendering/src/3DUtility.hpp
@@ -1,5 +1,8 @@
-struct Point
+sf::Vector2f rotatePoint(const sf::Vector2f &point, const float &theta)
 {
-	sf::CircleShape shape;
-	sf::Vector3f position;
-};
+	// Compute using 2D rotation matrix for angle theta in radius
+	sf::Vector2f result;
+	result.x = point.x * std::cosf(theta) - point.y * std::sinf(theta);
+	result.y = point.x * std::sinf(theta) + point.y * std::cosf(theta);
+	return result;
+}
--- 3D-Rendering/src/Main.cpp
+++ 3D-Rendering/src/Main.cpp
@@ -17,6 +17,37 @@ int main()
 	gridline.setOrigin(width / 2, 1);
 	gridline.setPosition(0, width / 2);
 
+	const int radius = 25;
+	const sf::Vector2f center(width / 2, width / 2);
+	// Origin at center coordinates
+	sf::Vector2f p1(-100, 100);
+	sf::Vector2f p2(100, 100);
+	sf::Vector2f p3(100, -100);
+	sf::Vector2f p4(-100, -100);
+
+	sf::CircleShape point1(radius);
+	point1.setOrigin(radius, radius);
+	point1.setFillColor(sf::Color::White);
+	point1.setPosition(center.x + p1.x, center.y - p1.y);
+
+	sf::CircleShape point2(radius);
+	point2.setOrigin(radius, radius);
+	point2.setFillColor(sf::Color::White);
+	point2.setPosition(center.x + p2.x, center.y - p2.y);
+
+	sf::CircleShape point3(radius);
+	point3.setOrigin(radius, radius);
+	point3.setFillColor(sf::Color::White);
+	point3.setPosition(center.x + p3.x, center.y - p3.y);
+
+	sf::CircleShape point4(radius);
+	point4.setOrigin(radius, radius);
+	point4.setFillColor(sf::Color::White);
+	point4.setPosition(center.x + p4.x, center.y - p4.y);
+
+	double angle = 0.001;
+
+
 	sf::Event event;
 	while (window.isOpen())
 	{
@@ -31,6 +62,19 @@ int main()
 					continue;
 			}
 		}
+
+
+		p1 = rotatePoint(p1, angle);
+		p2 = rotatePoint(p2, angle);
+		p3 = rotatePoint(p3, angle);
+		p4 = rotatePoint(p4, angle);
+		point1.setPosition(center.x + p1.x, center.y - p1.y);
+		point2.setPosition(center.x + p2.x, center.y - p2.y);
+		point3.setPosition(center.x + p3.x, center.y - p3.y);
+		point4.setPosition(center.x + p4.x, center.y - p4.y);
+
+
+
 
 		window.clear(sf::Color(15, 15, 15));
 
@@ -49,6 +93,10 @@ int main()
 			window.draw(gridline);
 		}
 
+		window.draw(point1);
+		window.draw(point2);
+		window.draw(point3);
+		window.draw(point4);
 		window.display();
 	}
 

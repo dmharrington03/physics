5d99e786ec7de85f00831f17acdd842edd034c9b
Daniel Harrington <dmharrington03@gmail.com>
2022-07-30 13:09:48 -0400

    draw lines method

--- Electric Field/main.js
+++ Electric Field/main.js
@@ -9,17 +9,19 @@ class PointCharge {
     // All calculations done in center-origin coordinates
     pos = createVector(0, 0);
     charge = 0;
-    radius = 30;
-    linesPerCharge = 6;
+    radius = 10;
+    linesPerCharge = 3;
     startPoints = [];
     target;
+    dR = 5;
 
     constructor(x, y, charge, target) {
+        // all parameters should be center-origin
         this.pos.x = x
         this.pos.y = y
         this.charge = charge;
         this.target = target; // p5 Vector
-        //TODO convert target to center-origin
+        this.createStartPoints();
     }
 
     createStartPoints() {
@@ -29,8 +31,18 @@ class PointCharge {
 
         for (let i = 0; i < n_lines; i++) {
             // Push cartesian coordinates to startPoints array
-            this.startPoints.push(p5.Vector.mult(createVector(cos(angle), sin(angle)), this.radius));
+            let theta = angle * i;
+            this.startPoints.push(p5.Vector.mult(createVector(cos(theta), sin(theta)), this.radius));
         }
+        console.log(this.startPoints);
+    }
+
+    getEField(displacement) {
+        // param displacement: p5.Vector
+        // returns p5.Vector
+
+        let EFieldMag = 10000 * this.charge / pow(displacement.mag(), 2);
+        return p5.Vector.mult(displacement.normalize(), EFieldMag);
     }
 
     drawLines() {
@@ -38,57 +50,73 @@ class PointCharge {
         /*  Loop until off-screen or at end region
             Calculate E field normal vector 
             Move dR * Ehat and make new vertex */
+        //TODO fix
+        for (let i = 0; i < this.startPoints.length; i++) {
+            
+            let pt = createVector(this.startPoints[i].x, this.startPoints[i].y);
+            console.log("pt" + i + " " + pt);
+            for (let i = 0; i < 10; i++) {
+                let EFieldUnit = this.getEField(p5.Vector.sub(pt, this.pos));
+                let newPt = p5.Vector.add(pt, p5.Vector.mult(EFieldUnit, this.dR));
+
+                let cPt = PointCharge.toCornerOrigin(pt);
+                let cNewPt = PointCharge.toCornerOrigin(newPt);
+                line(cPt.x, cPt.y, cNewPt.x, cNewPt.y);
+                // ellipse(cPt.x, cPt.y, 5, 5);
+                pt = newPt;
+            }
+            console.log("done")
+        }
     }
 
     isInBounds(positionVector) {
         // param positionVector: p5 Vector
         
-        // Check off-screen
-        let onScreen = (abs(positionVector.x) >= width / 2) || (abs(positionVector.y) >= height / 2);
+        // Check on-screen
+        let onScreen = (abs(positionVector.x) <= width / 2) || (abs(positionVector.y) <= height / 2);
 
-        // Check if in range of target
-        let displacement = p5.Vector.sub(this.target, positionVector);
-        let inRange = displacement.mag() < this.radius;
-        return onScreen || inRange;
+        // Check if not in range of target
+        // let displacement = p5.Vector.sub(this.target, positionVector);
+        // let notInRange = displacement.mag() > this.radius + 1;
+        return onScreen;
     }
 
     dispay() {
         // Draw to the canvas
         fill(250, 50, 50);
-        ellipse(this.pos.x, this.pos.y, this.radius, this.radius);
+        let cPos = PointCharge.toCornerOrigin(this.pos);
+        ellipse(cPos.x, cPos.y, this.radius * 2);
     }
 
-    static toCornerOrigin(a, b) {
+    static toCornerOrigin(a) {
         // Given a point in center-origin coordinates, convert to upper-corner center
-        return createVector(a + width / 2, height / 2 - b);
+        // param a: p5.Vector
+        return createVector(a.x + width / 2, height / 2 - a.y);
     }
 }
 
 
+let source;
 
 function setup() {
     createCanvas(width, height);
+    source = new PointCharge(0, 0, 2, createVector(500, 500));
+    noLoop();
 }
 
 function draw() {
+
     // Set origin to center of screen
     // translate(width/2, height/2); 
     // scale(1, -1);
 
-    var source = new PointCharge(200, 200, 3);
     background(220);
-    noStroke();
+    // noStroke();
+    strokeWeight(2);
     fill(250, 50, 50);
     // ellipse(width / 2, height / 2, 30, 30);
+    source.drawLines();
     source.dispay();
     
 }
 
-function getEField(charge) {
-    // param point: p5.Vector
-    // param charge: float
-    const displacement = createVector(mouseX, mouseY); // Assumes charge at (0, 0)
-    displacement.div(1000);
-    let EField = charge / pow(displacement.mag(), 2);
-    return EField;
-}

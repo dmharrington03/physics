9865f9c824d4bac92de91aec026f0a8b66cb84fe
Daniel Harrington <dmharrington03@gmail.com>
2022-09-25 22:06:23 -0400

    initial commit

---
+++ Fluids/src/main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+#include "headers.hpp"
+#include "funcs.cpp"
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(addWidth(80));
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}

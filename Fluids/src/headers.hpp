f01783f04002756b2133affb9cb182c8c0e634d6
Daniel Harrington <dmharrington03@gmail.com>
2022-10-07 23:33:49 -0400

    defined headers

--- Fluids/src/headers.hpp
+++ Fluids/src/headers.hpp
@@ -1,1 +1,10 @@
-float addWidth(float width1);
+void add_source(int N, float *x, float *s, float dt);
+void diffuse(int N, int b, float *x, float *x0, float diff, float dt);
+void advect(int N, int b, float *d, float *d0, float *u, float *v, float dt);
+void dens_step(int N, float *x, float *x0, float *u, float *v, float diff, float dt);
+void vel_step(int N, float *u, float *v, float *u0, float *v0, float visc, float dt);
+void project(int N, float *u, float *v, float *p, float *div);
+void set_bnd(int N, int b, float *x);
+void init_zero(int size, float *a);
+void fade_dens(int N, float *dens, float amt);
+float constrain(float n, float min, float max);
